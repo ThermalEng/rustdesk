61bc81f6e183301c7614eaa0291febd7b31c0dca
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-04-16 08:14:50 +0000

    安卓图标

--- res/gen_icon.sh
+++ res/gen_icon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for size in 16 32 64 128 256 512 1024; do
+for size in 48 72 96 144 192; do
     #inkscape -z -o $size.png -w $size -h $size icon.svg >/dev/null 2>/dev/null
-    convert icon.png -resize ${size}x${size} app_icon_$size.png
+    convert icon.png -resize ${size}x${size} ${size}x${size}.png
 done
 # from ImageMagick
 convert 16.png 32.png 48.png 128.png 256.png -colors 256 icon.ico
